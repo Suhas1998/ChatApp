f16d940bf2eba3fd53f9b104d5c383cd96dbfba0
Suhas1998 <suhas.edu23@gmail.com>
2020-08-01 18:59:37 +0530

    First Commit

---
+++ public/script.js
@@ -0,0 +1,50 @@
+const socket = io('http://localhost:8086')
+const messageForm = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt("Whats your name")
+appendMessage('You Joined')
+socket.emit('new-user',name)
+
+
+socket.on('chat-message', data =>{
+  appendMessage(`${data.name}` + ': '+ `${data.message}`)
+})
+
+socket.on('user-connected', name =>{
+  appendMessage(`${name} joined the room`)
+})
+
+socket.on('user-disconnected', name =>{
+  appendMessage(`${name} left the room`)
+})
+
+
+messageForm.addEventListener('submit', e =>{
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage('You: '+ message)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+
+function appendMessage(message){
+  const messageElement = document.createElement("div");
+  messageElement.className = "message-item pl-2"
+  messageElement.innerHTML = message
+  // const user = document.createElement("h5");
+  // user.innerHTML = message.name;
+  // user.className = "card-title";
+
+  // const msg = document.createElement("p");
+  // msg.className = "card-text";
+  // msg.innerHTML = message.message;
+
+  // messageElement.append(user);
+  // messageElement.append(msg);
+
+  messageContainer.append(messageElement);
+
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const io = require('socket.io')(8086)
+
+const users = {}
+
+io.on('connection', socket => {
+
+  socket.on('new-user', name =>{
+    console.log("New User Joined ")
+    users[socket.id] = name
+    console.log(name)
+    socket.broadcast.emit('user-connected',name)
+  })
+
+  socket.on('send-chat-message',message =>{
+    // emits the message to all the connect clients except the sender
+    console.log("Message Sent")
+    socket.broadcast.emit('chat-message',{message: message, name: users[socket.id]})
+  })
+
+  socket.on('disconnect', () =>{
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
